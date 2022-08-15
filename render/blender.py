5e43ea09da16cc7eea6b18a8f81a0ca6dfe27de6
zhang722 <1755169086@qq.com>
2022-08-15 10:31:20 +0800

    now we can see a cube moves in blender

--- render/blender.py
+++ render/blender.py
@@ -1,19 +1,46 @@
 import bpy
+import math
+import functools
 
 class Blender:
     def __init__(self):
         self.time = 0.0
+        self.data = {"t": [0, 0, 0], "q": [0, 0, 0], "delta_time": [0]}
+        self.iterNum = 0
+        self.N = 100
+        self.delta_time = 0.1
 
     # type data is dict 
-    def assignToBlender(self, data, name):
-        bpy.data.Objects[name].location[0] = data["t"][0]
-        bpy.data.Objects[name].location[1] = data["t"][1]
-        bpy.data.Objects[name].location[2] = data["t"][2]
+    def assignToBlender(self, name):
+        bpy.data.objects[name].location[0] = self.data["t"][0]
+        bpy.data.objects[name].location[1] = self.data["t"][1]
+        bpy.data.objects[name].location[2] = self.data["t"][2]
 
-        bpy.data.Objects[name].rotation_quaternion[0] = data["q"][0]
-        bpy.data.Objects[name].rotation_quaternion[1] = data["q"][1]
-        bpy.data.Objects[name].rotation_quaternion[2] = data["q"][2]
-        bpy.data.Objects[name].rotation_quaternion[3] = data["q"][3]
-        self.delta_time = data["delta_time"]
+        bpy.data.objects[name].rotation_euler[0] = self.data["q"][0]
+        bpy.data.objects[name].rotation_euler[1] = self.data["q"][1]
+        bpy.data.objects[name].rotation_euler[2] = self.data["q"][2]
+
+    def update(self):
+        self.data["t"] = [10*math.cos(6.28 / self.N * self.iterNum), 10*math.sin(6.28 / self.N * self.iterNum), 0]
+        self.data["q"] = [0, 0, 6.28 / self.N * self.iterNum]
+        self.iterNum += 1
 
 
+def renderUpdate(blender):
+    blender.assignToBlender("Cube")
+    blender.update()
+    if blender.iterNum > 99:
+        raise Exception("stop iter")
+    return blender.delta_time
+
+
+def main():
+    blender = Blender()
+    bpy.app.timers.register(functools.partial(renderUpdate, blender))
+
+
+if __name__=="__main__":
+    try:
+        main()  
+    except Exception:
+        print("done")
